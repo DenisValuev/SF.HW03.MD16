1e2f11718aa35df8efe2b8fefb06d85449ba5a04
DenisValuev <valuev.denis91@mail.ru>
2024-10-30 15:18:44 +0300

    Добавьте файлы проекта.

---
+++ SF.HW03.MD16/Models/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.HW03.MD16.Models
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
